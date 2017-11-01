6d9e5c206c59038971b32428bc8fb3e48eb2be05
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2017-11-01 09:16:38 -0400

    Update: add unmapped reads flag

--- 3-athaliana_quant.sh
+++ 3-athaliana_quant.sh
@@ -7,7 +7,8 @@ salmon quant -i athaliana/ref/athaliana_normal_index -l A \
         -1 athaliana/data/DRR016125/DRR016125_1.fastq.gz \
         -2 athaliana/data/DRR016125/DRR016125_2.fastq.gz \
         -p 8 -o athaliana/quants/DRR016125_quant \
-        --gcBias --seqBias --numBootstraps 1000 --biasSpeedSamp 5
+        --gcBias --seqBias --numBootstraps 1000 --biasSpeedSamp 5 \
+        --writeUnmappedNames
 
 
 # single-end example, read length = 36 so we use the "short" index
@@ -16,7 +17,7 @@ salmon quant -i athaliana/ref/athaliana_normal_index -l A \
 salmon quant -i athaliana/ref/athaliana_short_index -l A \
 		-r athaliana/data/SRR074262/SRR074262.fastq.gz \
 		-p 8 -o athaliana/quants/SRR074262_quant \
-		--seqBias --numBootstraps 1000
+		--seqBias --numBootstraps 1000 --writeUnmappedNames
 
 # multi-sample experiment, read length = 50 so we use the "short" index
 # do not correct for GC bias (single-end library), correct for sequence-specific
@@ -30,5 +31,5 @@ do
 	salmon quant -i athaliana/ref/athaliana_short_index -l A \
 			-r athaliana/data/${samp}/${samp}.fastq.gz \
 			-p 8 -o athaliana/quants/PRJNA408323/${samp}_quant \
-			--seqBias --numBootstraps 1000
+			--seqBias --numBootstraps 1000 --writeUnmappedNames
 done
